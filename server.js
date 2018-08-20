6981e3af87142f4d3c62077c65deda97daaae614
omkar98 <edu.omkar@gmail.com>
2018-08-20 09:26:34 +0530

    Logger Maintenence File

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 var app = express();
 //request: data you get from the client
 //response: how you handle the incoming requests with the data available to you
 hbs.registerPartials(__dirname+'/views/partials')
-
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('convertToUpperCase',(text)=>{
+  return text.toUpperCase();
+});
 //app.set('key', 'value'); key is the value you want to set and value is the value you want to use
 app.set('view engine','hbs');
 
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', (err)=>{
+    if(err){
+      console.log('Cannot append the file');
+    }
+  });
+  next()
+});
+
+app.use((req,res,next)=>{
+  res.render('maintenence.hbs');
+});
+
 app.use(express.static(__dirname+'/public'));//middleware
+
 app.get('/',(req,res)=>{
   res.render('home.hbs',{
     pageTitle: 'Home Page',
     pageDesc: 'This is my Home page',
-    copyright_year: new Date().getFullYear()
+    //copyright_year: new Date().getFullYear()
   });
 });
 
@@ -22,7 +44,7 @@ app.get('/about', (req, res)=>{
   res.render('about.hbs',{
     pageTitle: 'About Us',
     pageDesc: 'This is my about us page',
-    copyright_year: new Date().getFullYear()
+    //copyright_year: new Date().getFullYear()
   });
 });
 
